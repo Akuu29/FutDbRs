b55b1629f9cfdd5c79ffd7602fbacab1515da939
Akuu29 <owner.akiu0529@gmail.com>
2022-02-25 23:41:23 +0900

    Update Update src/batch/Clubs/clubs.rs

--- src/batch/Clubs/clubs.rs
+++ src/batch/Clubs/clubs.rs
@@ -1,25 +1,38 @@
-extern crate reqwest;
+// extern crate reqwest;
 use reqwest::header;
 use dotenv::dotenv;
 use std::env;
+// use std::collections::HashMap;
 
-fn http_get_clubs() -> Result<(), Box<dyn std::error::Error>> {
+async fn http_get_clubs() -> Result<(), Box<dyn std::error::Error>> {
     dotenv().ok();
     let token = env::var("TOKEN").expect("TOKEN is not found").parse().unwrap();
     let mut headers = header::HeaderMap::new();
     headers.insert("accept", "applicaion/json".parse().unwrap());
     headers.insert("X-AUTH-TOKEN", token);
 
-    let client = reqwest::Client::new();
-    let resp = client
+    println!("{}", "aaaaaaaaaaaaaaaaaaaaa");
+
+    let client = reqwest::Client::builder().build()?;
+    let res = client
       .get("https://futdb.app/api/clubs?page=1&limit=20")
-      .headers(headers);
-    println!("{:#?}", resp);
+      .headers(headers)
+      .send()
+      .await?
+      .text()
+      .await?;
+
+    println!("{:?}", res);
+
+    // let context = res
+    //   .json::<HashMap<String, String>>()
+    //   .await?;
+
+    // println!("{:?}", context);
 
     Ok(())
 }
 
-
 fn main() {
-   http_get_clubs();
+    http_get_clubs();
 }
