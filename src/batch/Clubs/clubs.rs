e3cfe61e2b593b633cbc190dde8c1cb3891cd5ea
Akuu29 <owner.akiu0529@gmail.com>
2022-04-02 04:43:45 +0900

    fix fix clubs.rs.

--- src/batch/Clubs/clubs.rs
+++ src/batch/Clubs/clubs.rs
@@ -1,38 +1,36 @@
-// extern crate reqwest;
 use reqwest::header;
 use dotenv::dotenv;
 use std::env;
-// use std::collections::HashMap;
 
-async fn http_get_clubs() -> Result<(), Box<dyn std::error::Error>> {
+type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
+
+async fn http_get_clubs() -> Result<String> { // Result<(), Box<dyn std::error::Error>>
     dotenv().ok();
-    let token = env::var("TOKEN").expect("TOKEN is not found").parse().unwrap();
+
+    let token = env::var("TOKEN")
+      .expect("TOKEN is not found")
+      .parse()
+      .unwrap();
+
+    // headers
     let mut headers = header::HeaderMap::new();
     headers.insert("accept", "applicaion/json".parse().unwrap());
     headers.insert("X-AUTH-TOKEN", token);
-
-    println!("{}", "aaaaaaaaaaaaaaaaaaaaa");
 
     let client = reqwest::Client::builder().build()?;
     let res = client
       .get("https://futdb.app/api/clubs?page=1&limit=20")
       .headers(headers)
       .send()
-      .await?
-      .text()
       .await?;
 
-    println!("{:?}", res);
+    let body = res.text().await?;
 
-    // let context = res
-    //   .json::<HashMap<String, String>>()
-    //   .await?;
-
-    // println!("{:?}", context);
-
-    Ok(())
+    Ok(body)
 }
 
-fn main() {
-    http_get_clubs();
+#[tokio::main]
+async fn main() {
+    let res = http_get_clubs().await.unwrap();
+    println!("{}", res);
 }
