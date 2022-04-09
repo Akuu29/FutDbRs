ed0f31652dc425b4ae40b860d47a1566c23c7592
Akuu29 <owner.akiu0529@gmail.com>
2022-04-09 21:00:57 +0900

    update Update src/clubs/specific_club/get_club.rs, src/clubs/all_clubs/get_clubs.rs. Added res_type to fn http_request, so that res_type is added as an argument in the fn http_get_clubs, http_get_club where http_request is used.

--- src/clubs/all_clubs/get_clubs.rs
+++ src/clubs/all_clubs/get_clubs.rs
@@ -1,12 +1,14 @@
 use crate::request;
 use crate::clubs::all_clubs::model_clubs::Clubs;
+use crate::mime_type::MimeType;
 
 type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
 
 // クラブ全件取得
 pub async fn http_get_clubs() -> Result<Clubs> {
     let url = String::from("https://futdb.app/api/clubs?page=1&limit=20");
-    let body = request::http_request(url)
+    let res_type = MimeType::Json;
+    let body = request::http_request(url, res_type)
       .await?
       .json::<Clubs>()// responseをjsonに変換
       .await?;
--- src/clubs/specific_club/get_club.rs
+++ src/clubs/specific_club/get_club.rs
@@ -1,11 +1,13 @@
 use crate::request;
 use crate::clubs::specific_club::model_club::Clubs;
+use crate::mime_type::MimeType;
 
 type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
 
 pub async fn http_get_club(id: u32) -> Result<Clubs> {
     let url = format!("https://futdb.app/api/clubs/{}", id);
-    let body = request::http_request(url)
+    let res_type = MimeType::Json;
+    let body = request::http_request(url, res_type)
       .await?
       .json::<Clubs>()
       .await?;
