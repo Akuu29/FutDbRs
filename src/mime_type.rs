8c14b9be4a5ffdec19aa8114436fb91570e07d1a
Akuu29 <owner.akiu0529@gmail.com>
2022-04-09 20:52:43 +0900

    update Update request.rs. Added argument res_type to fn http_request. The res_type is used to determine the mime type of httprequest headder. Added src/mime_type.rs and defined the enum MimeType.

---
+++ src/mime_type.rs
@@ -0,0 +1,4 @@
+pub enum MimeType {
+    Json,
+    Image
+}
--- src/request.rs
+++ src/request.rs
@@ -1,11 +1,17 @@
 use reqwest::{Response, header, Client};
 use dotenv::dotenv;
 use std::env;
+use crate::mime_type::MimeType;
 
 type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
 
-pub async fn http_request(url: String) -> Result<Response> {
+pub async fn http_request(url: String, res_type: MimeType) -> Result<Response> {
     dotenv().ok();
+
+    let mime_type = match res_type {
+        MimeType::Json => "application/json",
+        MimeType::Image => "image/png",
+    };
 
     let token = env::var("TOKEN")
       .expect("TOKEN is not found")
@@ -13,7 +19,7 @@ pub async fn http_request(url: String) -> Result<Response> {
       .unwrap();
     
     let mut headers = header::HeaderMap::new();
-    headers.insert("accept", "application/json".parse().unwrap());
+    headers.insert("accept", mime_type.parse().unwrap());
     headers.insert("X-AUTH-TOKEN", token);
 
     let client = Client::builder().build()?;
