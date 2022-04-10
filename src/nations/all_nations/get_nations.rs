4bcb63f00edb261ffa98a167607d041d07a67e10
Akuu29 <owner.akiu0529@gmail.com>
2022-04-10 23:51:48 +0900

    add Add src/nations/all_nations.

---
+++ src/nations/all_nations/get_nations.rs
@@ -0,0 +1,16 @@
+use crate::nations::all_nations::model_nations::Nations;
+use crate::mime_type::MimeType;
+use crate::request::http_request;
+
+type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
+
+pub async fn http_get_nations() -> Result<Nations> {
+    let url = format!("https://futdb.app/api/nations?page=1&limit=20");
+    let res_type = MimeType::Json;
+    let body = http_request(url, res_type)
+      .await?
+      .json::<Nations>()
+      .await?;
+    
+    Ok(body)
+}
---
+++ src/nations/all_nations/mod.rs
@@ -0,0 +1,3 @@
+mod get_nations;
+pub use get_nations::http_get_nations;
+mod model_nations;
