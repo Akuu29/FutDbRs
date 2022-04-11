05f8b010a15a116b83313475b2faf5823173ac62
Akuu29 <owner.akiu0529@gmail.com>
2022-04-11 22:14:22 +0900

    add Add src/players/specific_player/

---
+++ src/players/specific_player/get_player.rs
@@ -0,0 +1,17 @@
+use crate::request;
+use crate::players::specific_player::model_player::Players;
+use crate::mime_type::MimeType;
+
+type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
+
+pub async fn http_get_player(id: u32) -> Result<Players> {
+    let url = format!("https://futdb.app/api/players/{}", id);
+    let res_type = MimeType::Json;
+
+    let body = request::http_request(url, res_type)
+        .await?
+        .json::<Players>()
+        .await?;
+    
+    Ok(body)
+}
---
+++ src/players/specific_player/mod.rs
@@ -0,0 +1,3 @@
+mod get_player;
+pub use get_player::http_get_player;
+mod model_player;
