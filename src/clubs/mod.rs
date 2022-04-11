bcf7683a4da01cd819cda7ee8d05f68fa95c6c84
Akuu29 <owner.akiu0529@gmail.com>
2022-04-12 00:11:46 +0900

    update Update src/clubs/mod.rs

--- src/clubs/mod.rs
+++ src/clubs/mod.rs
@@ -1,3 +1,6 @@
-mod get_clubs;
-pub use get_clubs::http_get_clubs;
-pub mod model_club;
+mod all_clubs;
+pub use all_clubs::http_get_clubs;
+mod specific_club;
+pub use specific_club::http_get_club;
+// mod club_image;
+// pub use club_image::get_club_image::http_get_club_image;
